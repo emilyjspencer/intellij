63f30d9fd912aacf80b809f58aaab05e83025316
emilyjspencer <emilyspencer89@hotmail.co.uk>
2021-02-19 23:58:43 +0000

    Adds Banking

---
+++ src/BankAccountTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+
+public class BankAccountTest {
+
+    @org.junit.Test
+    public void deposit() {
+        BankAccount bankaccount = new BankAccount("Bog", "Creature", 100.0);
+        double result = bankaccount.deposit(20.0, true);
+        assertEquals(120.00, result, 0.1 );
+    }
+
+    @org.junit.Test
+    public void withdraw() {
+        BankAccount bankaccount = new BankAccount("Bog", "Creature", 100.0);
+        double result = bankaccount.withdraw(20.0, true);
+        assertEquals(80.0, result, 0.1);
+    }
+
+    @org.junit.Test
+    public void getBalance() {
+        BankAccount bankaccount = new BankAccount("Bog", "Creature", 60.0);
+        double result = bankaccount.getBalance();
+        assertEquals(60.0, result, 0.1);
+    }
+}
