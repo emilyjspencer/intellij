68140173c907ba414ad3a5cb6e6b0fa4a17806d3
emilyjspencer <emilyspencer89@hotmail.co.uk>
2021-02-20 00:18:05 +0000

    Modifies names

--- src/BankAccountTest.java
+++ src/BankAccountTest.java
@@ -4,21 +4,21 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void deposit() {
-        BankAccount bankaccount = new BankAccount("Bog", "Creature", 100.0);
+        BankAccount bankaccount = new BankAccount("Jane", "Doe", 100.0);
         double result = bankaccount.deposit(20.0, true);
         assertEquals(120.00, result, 0.1 );
     }
 
     @org.junit.Test
     public void withdraw() {
-        BankAccount bankaccount = new BankAccount("Bog", "Creature", 100.0);
+        BankAccount bankaccount = new BankAccount("Joe", "Bloggs", 100.0);
         double result = bankaccount.withdraw(20.0, true);
         assertEquals(80.0, result, 0.1);
     }
 
     @org.junit.Test
     public void getBalance() {
-        BankAccount bankaccount = new BankAccount("Bog", "Creature", 60.0);
+        BankAccount bankaccount = new BankAccount("Jane", "Doe", 60.0);
         double result = bankaccount.getBalance();
         assertEquals(60.0, result, 0.1);
     }
