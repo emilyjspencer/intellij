e69aee0eeb514ae397907c245b73f23505378e34
emilyjspencer <emilyspencer89@hotmail.co.uk>
2021-02-20 00:55:18 +0000

    Refactors BankAccountTest to use BeforeEach to DRY up code

--- src/BankAccountTest.java
+++ src/BankAccountTest.java
@@ -4,32 +4,33 @@ import static org.junit.Assert.*;
 
 public class BankAccountTest {
 
+   BankAccount bankaccount;
 
+    @org.junit.Before
+    public void setUp() throws Exception {
+        bankaccount = new BankAccount("Jane", "Doe", 100.0, BankAccount.CHECKINGACCOUNT);
+    }
 
     @org.junit.Test
     public void deposit() {
-        BankAccount bankaccount = new BankAccount("Jane", "Doe", 100.0, BankAccount.CHECKINGACCOUNT);
         double result = bankaccount.deposit(20.0, true);
         assertEquals(120.00, result, 0.1 );
     }
 
     @org.junit.Test
     public void withdraw() {
-        BankAccount bankaccount = new BankAccount("Joe", "Bloggs", 100.0, BankAccount.CHECKINGACCOUNT);
         double result = bankaccount.withdraw(20.0, true);
         assertEquals(80.0, result, 0.1);
     }
 
     @org.junit.Test
     public void getBalance() {
-        BankAccount bankaccount = new BankAccount("Jane", "Doe", 60.0, BankAccount.CHECKINGACCOUNT);
         double result = bankaccount.getBalance();
-        assertEquals(60.0, result, 0.1);
+        assertEquals(100.0, result, 0.1);
     }
 
     @org.junit.Test
     public void isCheckingAccount_true() {
-        BankAccount bankaccount = new BankAccount("Jane", "Doe", 120.0, BankAccount.CHECKINGACCOUNT);
         assertTrue( bankaccount.isCheckingAccount());
     }
 }
